f77ba89d502e6dbe6fd619a55173ad06aaf2c75e
rosanaramo <rosanaramo4@gmail.com>
2026-07-09 16:41:26 -0300

    changed the fixture

--- helpers/authentication.js
+++ helpers/authentication.js
@@ -1,13 +1,15 @@
 
 require('dotenv').config()
 const request = require('supertest');
+const credentials = require('../fixtures/credentials.json')
+
 const getToken = async (usuario, senha) =>{
+    const bodyCredentials= {...credentials}
     const response = await request(process.env.BASE_URL)
                     .post('/auth')
                     .set('Content-Type','application/json')
                     .send({
-                        "username" : usuario,
-                        "password" : senha
+                        bodyCredentials
                     })
         
     return response.body.token
