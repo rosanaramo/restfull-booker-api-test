d310de65ef6d06304a4be59781d30785e468cfc6
rosanaramo <rosanaramo4@gmail.com>
2026-07-20 18:01:42 -0300

    changed tests

--- test/booking/patch-booking.test.js
+++ test/booking/patch-booking.test.js
@@ -22,7 +22,7 @@ describe('Patch/booking/',()=>{
     });
 
     describe('Authorization', ()=>{
-        it('Should return 403 forbidden for invalid token',async ()=>{
+        it('Should return 403 when token is invalid',async ()=>{
             
                 const response = await request(process.env.BASE_URL)
                 .patch(`/booking/${bookingId}`)
@@ -138,6 +138,26 @@ describe('Patch/booking/',()=>{
                expect(persistedBooking.body.additionalneeds).to.be.equal(bookingBody.additionalneeds)
             });
 
+        it('Should update multiple fields', async ()=>{
+                
+            const bookingBody ={
+                additionalneeds: "Breakfast and A/C",
+                firstname: 'Loren',
+                totalprice: 909
+            }
+                await request(process.env.BASE_URL)
+                .patch(`/booking/${bookingId}`)
+                .set('Accept', 'application/json')
+                .set('Cookie', `token=${token}`)
+                .send(bookingBody)
+                .expect(200)
+    
+               const persistedBooking = await getBooking(bookingId);                
+               expect(persistedBooking.body.additionalneeds).to.be.equal(bookingBody.additionalneeds)
+               expect(persistedBooking.body.firstname).to.be.equal(bookingBody.firstname)
+               expect(persistedBooking.body.totalprice).to.be.equal(bookingBody.totalprice)
+            });
+
     });
 
     describe('Headers Validation',()=>{
@@ -155,7 +175,7 @@ describe('Patch/booking/',()=>{
         
                 it('Should return header application/json; charset=utf-8', async ()=>{
                     const response = await request(process.env.BASE_URL)
-                    .put(`/booking/${bookingId}`)
+                    .patch(`/booking/${bookingId}`)
                     .set('Accept', 'application/json')
                     .set('Cookie', `token=${token}`)
                     .send(bookingBody)
@@ -187,8 +207,10 @@ describe('Patch/booking/',()=>{
         it('Should return 400 when updating checkin with null value', async ()=>{
               //BUG: API returns 200 server error instead of 400
             const bookingBody ={
-                checkin: null
-            }
+                            bookingdates:{
+                                   checkin:null,
+                            }
+                        }
     
             const response = await request
             (process.env.BASE_URL)
@@ -201,12 +223,16 @@ describe('Patch/booking/',()=>{
     
         it('Should return 400 when updating checkout with null value',  async () =>{
 
+            // Bug: Returns 200 instead of 400
              const bookingBody ={
-                checkout: null
-            }
+                            bookingdates:{
+                                  
+                                   checkout: null 
+                            }
+                        }
             
             const response = await request(process.env.BASE_URL)
-            .put(`/booking/${bookingId}`)
+            .patch(`/booking/${bookingId}`)
             .set('Accept', 'application/json')
             .set('Cookie', `token=${token}`)
             .send(bookingBody)
@@ -215,13 +241,13 @@ describe('Patch/booking/',()=>{
         })
     
         it('Should return 400 when update the depositpaid with a non-boolean value ', async () => {
-          
+          // Bug: returns 200 instead of 400
             const bookingBody ={
-                depositpaid: null
+                depositpaid: "123"
             }
     
             const response = await request(process.env.BASE_URL)
-            .put(`/booking/${bookingId}`)
+            .patch(`/booking/${bookingId}`)
             .set('Accept', 'application/json')
             .set('Cookie', `token=${token}`)
             .send(bookingBody)
@@ -249,7 +275,7 @@ describe('Patch/booking/',()=>{
                         expect(persistedBooking.body.totalprice).to.be.deep.equal(bookingBody.totalprice);
                     });
             
-                    it('Should return 400 when updpating checkout before the checkin', async () =>{
+                    it('Should return 400 when updating checkout before the checkin', async () =>{
                         // BUG: API allows registering a booking with checkout before the checkin
                         
                         let checkout =subDaysFromCurrentDate(7)
@@ -315,7 +341,7 @@ describe('Patch/booking/',()=>{
                     // .expect(200)
         
                      const persistedBooking = await getBooking(bookingId);                
-                     expect(persistedBooking.body).to.be.equal(updatedBooking)
+                     expect(persistedBooking.body).to.be.deep.equal(updatedBooking)
         
                 });
 
@@ -323,9 +349,49 @@ describe('Patch/booking/',()=>{
 
     describe('Security Tests', ()=>{
 
+        it('Should reject HTML script',async ()=>{
+                            //BUG: API accepts html scripts and returns 200 instead of return 400
+                            bookingBody.firstname = "<script>alert(1)</script>"
+                
+                            const response = await request(process.env.BASE_URL)
+                            .patch(`/booking/${bookingId}`)
+                            .set('Cookie',`token=${token}`)
+                            .set('Accept', 'application/json')
+                            .send(bookingBody)
+                            .expect(400)
+                        });
+                
+                        it('Should reject sql injection', async ()=>{
+                            // BUG: API accepts sql injection and returns 200 instead reject and return 400
+                            bookingBody.firstname = "'; DROP TABLE booking; --";
+                            const response = await request(process.env.BASE_URL)
+                            .patch(`/booking/${bookingId}`)
+                            .set('Cookie', `token=${token}`)
+                            .set('Accept', 'application/json')
+                            .send(bookingBody)
+                            .expect(400)
+                            
+                        })
+
     });
 
     describe('Idempotency',()=>{
+
+        it('Should be idempotent when updating a booking', async ()=>{
+                    
+                        for(let i =0; i<2; i++ ){
+                            
+                            await request(process.env.BASE_URL)
+                            .patch(`/booking/${bookingId}`)
+                            .set('Accept', 'application/json')
+                            .set('Cookie', `token=${token}`)
+                            .send(bookingBody)
+                            .expect(200)
+                        }
+        
+                    const response = await getBooking(bookingId);                
+                    expect(response.body).to.be.deep.equal(bookingBody)
+                });
 
     })
     
